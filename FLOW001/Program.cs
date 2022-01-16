2edf75403ed8a07154746cf5ecdf5fbf6f496cbf
CrazyKoodaa <crazykoodaa@vivaldi.net>
2022-01-16 16:01:22 +0100

    FLOW001

---
+++ FLOW001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FLOW001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                string[] lines = Console.ReadLine().Split(' ');
+                Console.WriteLine(int.Parse(lines[0]) + int.Parse(lines[1]));
+            }
+        }
+    }
+}
