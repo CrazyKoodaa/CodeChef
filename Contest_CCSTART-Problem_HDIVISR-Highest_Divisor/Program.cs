d032d2798376a8a1e207eec8108eed2d5f8697ad
CrazyKoodaa <crazykoodaa@vivaldi.net>
2022-01-22 16:47:33 +0100

    Contest Code:CCSTART Problem Code:HDIVISR

---
+++ Contest_CCSTART-Problem_HDIVISR-Highest_Divisor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Test
+{
+	public static void Main()
+	{
+		// your code goes here
+		int iInputNumber;
+
+		iInputNumber = Convert.ToInt32(Console.ReadLine());
+
+		for (int i = 10; i >= 1; i--)
+		{
+			if (iInputNumber % i == 0)
+			{
+				Console.WriteLine(i);
+				break;
+			}
+		}
+	}
+}
